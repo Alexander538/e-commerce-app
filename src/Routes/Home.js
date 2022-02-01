a70ee9bbe688205be9467368377c8881aa29bee2
Alexander538 <alexanderferro19@gmail.com>
2022-02-01 17:32:12 -0500

    product data added

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -1,10 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Layout from '../Components/Layout';
+import { collection, addDoc, getDocs } from 'firebase/firestore';
+import fireDB from '../fireConfig';
+import { hatProducts } from '../data/hatProducts';
 
 function Home() {
-  return <Layout>
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    getData()
+  }, [])
+
+  async function getData() {
+    try {
+      const products = await getDocs(collection(fireDB, 'products'));
+      const productsArray = [];
+      products.forEach((doc) => {
+        const obj = {
+          id: doc.id,
+          ...doc.data(),
+        };
+
+        productsArray.push(obj);
+      });
+
+      setProducts(productsArray);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  function addProductsData() {
+    hatProducts.map(async (product) => {
+      try {
+        await addDoc(collection(fireDB, 'products'), product);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  }
+
+  return (
+    <Layout>
       <h1>Home</h1>
-  </Layout>;
+    </Layout>
+  );
 }
 
 export default Home;
---
+++ src/data/hatProducts.js
@@ -0,0 +1,82 @@
+export const hatProducts = [
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+  {
+    price: 20,
+    description: 'this is a very nice hat',
+    name: 'hat',
+    category: 'winter',
+    imageURL:
+      'https://image.shutterstock.com/image-photo/purple-hat-isolate-on-white-260nw-63133237.jpg',
+  },
+];
