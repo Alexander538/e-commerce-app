cc1e45436dce61c3a734e556a50e5cacbe7a5bd9
Alexander538 <alexanderferro19@gmail.com>
2022-02-01 14:01:26 -0500

    routing and basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Home from './Routes/Home';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
+import LoginPage from './Routes/LoginPage';
+import RegisterPage from './Routes/RegisterPage';
+import ProductInfo from './Routes/ProductInfo';
+import CartPage from './Routes/CartPage';
+ 
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' exact element={<Home />} />
+          <Route path='/login' exact element={<LoginPage />} />
+          <Route path='/register' exact element={<RegisterPage />} />
+          <Route path='/productinfo' exact element={<ProductInfo />} />
+          <Route path='/cart' exact element={<CartPage />} />
+
+
+
+        </Routes>
+      
+      
+      </BrowserRouter>
+
+
+
     </div>
   );
 }
---
+++ src/Routes/ProductInfo.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from '../Components/Layout';
+
+function ProductInfo() {
+  return <Layout>
+      <h1>ProductInfo</h1>
+  </Layout>;
+}
+
+export default ProductInfo;
