fe84cc1244449591011f41e125a4928da1b95988
Alexander538 <alexanderferro19@gmail.com>
2022-02-04 13:11:58 -0500

    added checkout modal

--- src/Routes/CartPage.js
+++ src/Routes/CartPage.js
@@ -1,30 +1,71 @@
 import React, { useEffect, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import { FaTrash } from 'react-icons/fa';
 import { useSelector, useDispatch } from 'react-redux';
 import Layout from '../Components/Layout';
+import { addDoc, collection } from 'firebase/firestore';
+import fireDB from '../fireConfig';
+import { toast } from 'react-toastify';
 
 function CartPage() {
   const { cartItems } = useSelector((state) => state.cartReducer);
   const [total, setTotal] = useState(0);
   const dispatch = useDispatch();
+  const [show, setShow] = useState(false);
 
-  useEffect(()=> {
-    let temp = 0;
-    cartItems.forEach((cartItem) => {
-      temp = temp+cartItem.price
-    })
-    setTotal(temp)
-  },[cartItems])
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [name, setName] = useState('');
+  const [address, setAddress] = useState('');
+  const [zipCode, setZipCode] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    localStorage.setItem('cartItems' , JSON.stringify(cartItems));
+    let temp = 0;
+    cartItems.forEach((cartItem) => {
+      temp = temp + cartItem.price;
+    });
+    setTotal(temp);
+  }, [cartItems]);
+
+  useEffect(() => {
+    localStorage.setItem('cartItems', JSON.stringify(cartItems));
   }, [cartItems]);
   const deleteFromCart = (product) => {
     dispatch({ type: 'DELETE_FROM_CART', payload: product });
   };
+  const placeOrder = async () => {
+    const addressInfo = {
+      name,
+      address,
+      zipCode,
+      phoneNumber,
+    };
+    console.log(addressInfo);
+
+    const orderInfo = {
+      cartItems,
+      addressInfo,
+      email: JSON.parse(localStorage.getItem('currentUser')).user.email,
+      userid: JSON.parse(localStorage.getItem('currentUser')).user.uid,
+    };
+
+    try {
+      setLoading(true);
+      const result = await addDoc(collection(fireDB, 'orders'), orderInfo);
+      setLoading(false);
+      toast.success('Order placed successfully');
+      handleClose();
+    } catch (error) {
+      setLoading(false);
+      toast.error('Order failed');
+    }
+  };
 
   return (
-    <Layout>
+    <Layout loading={loading}>
       <table className='table mt-3'>
         <thead>
           <tr>
@@ -45,7 +86,7 @@ function CartPage() {
                 <td>{item.name}</td>
                 <td>$ {item.price}</td>
                 <td>
-                  <FaTrash onClick={()=>deleteFromCart(item)} />
+                  <FaTrash onClick={() => deleteFromCart(item)} />
                 </td>
               </tr>
             );
@@ -57,8 +98,66 @@ function CartPage() {
         <h1 className='total-amount'>cart total: $ {total}</h1>
       </div>
       <div className='d-flex justify-content-end mt-3'>
-        <button>place order</button>
+        <button onClick={handleShow}>place order</button>
       </div>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Please provide your shipping information.</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {' '}
+          <div className='register-form'>
+            <h2>Register</h2>
+
+            <hr />
+
+            <input
+              type='text'
+              className='form-control'
+              placeholder='name'
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+            <textarea
+              rows={3}
+              type='text'
+              className='form-control'
+              placeholder='address'
+              value={address}
+              onChange={(e) => {
+                setAddress(e.target.value);
+              }}
+            />
+            <input
+              type='number'
+              className='form-control'
+              placeholder='zip code'
+              value={zipCode}
+              onChange={(e) => {
+                setZipCode(e.target.value);
+              }}
+            />
+            <input
+              type='number'
+              className='form-control'
+              placeholder='phone number'
+              value={[phoneNumber]}
+              onChange={(e) => {
+                setPhoneNumber(e.target.value);
+              }}
+            />
+
+            <hr />
+          </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <button onClick={handleClose}>Close</button>
+          <button onClick={placeOrder}>Order</button>
+        </Modal.Footer>
+      </Modal>
     </Layout>
   );
 }
